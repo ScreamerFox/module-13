018ebc87b46fcaeb94415ce431ca248bc632614b
ScreamerFox <142076193+ScreamerFox@users.noreply.github.com>
2024-10-02 10:53:13 +0500

    Create module_13_3.py

---
+++ module_13_3.py
@@ -0,0 +1,25 @@
+from aiogram import Dispatcher, Bot, executor, types
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+import asyncio
+import data
+
+
+
+api = f'{data.token_LB}'  # Ключ Telegram-бота
+bot = Bot(token=api)  # Обект бота
+# Диспатчер для обработки событий
+dp = Dispatcher(bot, storage=MemoryStorage())
+
+
+@dp.message_handler(commands=['start'])
+async def start(message):
+    await message.answer("Привет! Я бот помогающий твоему здоровью.")
+
+
+@dp.message_handler()
+async def all_massages(message):
+    await message.answer("Введите команду /start, чтобы начать общение.")
+
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
